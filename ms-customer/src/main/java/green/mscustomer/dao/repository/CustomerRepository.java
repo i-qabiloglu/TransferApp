613c07750003f6d6d1033ab0d84621cbcc1cdaba
Ibrahim <i.qabilogl@gmail.com>
2022-07-28 16:22:15 +0400

    add entity and repository

---
+++ ms-customer/src/main/java/green/mscustomer/dao/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package green.mscustomer.dao.repository;
+
+import green.mscustomer.dao.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+}
